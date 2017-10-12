dbf91701eb0b1d9938d9bd879ec2c58d2d580c13
FredericDT <frederic.dt.twh@gmail.com>
2017-10-12 19:05:18 +0800

    after read requirements

--- C01E01/Problem05.c
+++ C01E01/Problem05.c
@@ -6,31 +6,31 @@ int main() {
 	 * This program is intend to solve the problem on page 4
 	 * @author FredericDT
 	 */ 
-	char character = '0';
-	short shortInteger = 0;
-	int integer = 0;
-	long longInteger = 0;
-	long long longLongInteger = 0;
-	float floatNumber = 0.0;
-	double doubleNumber = 0.0;
+	char a = '0';
+	short b = 0;
+	int c = 0;
+	long d = 0;
+	long long e = 0;
+	float f = 0.0;
+	double g = 0.0;
 	printf("Please input seven parts: \n");
 	scanf("%c%hi%i%li%lli%f%lf", 
-		&character, 
-		&shortInteger,
-		&integer,
-		&longInteger,
-		&longLongInteger,
-		&floatNumber,
-		&doubleNumber
+		&a, 
+		&b,
+		&c,
+		&d,
+		&e,
+		&f,
+		&g
 	);
 	//carry these sizes of those variables
-	int sizeOfChar = sizeof(char);
-	int sizeOfShort = sizeof(short);
-	int sizeOfInt = sizeof(int);
-	int sizeOfLong = sizeof(long);
-	int sizeOfLongLong = sizeof(long long);
-	int sizeOfFloat = sizeof(float);
-	int sizeOfDouble = sizeof(double);
+	int sizeOfChar = sizeof(a);
+	int sizeOfShort = sizeof(b);
+	int sizeOfInt = sizeof(c);
+	int sizeOfLong = sizeof(d);
+	int sizeOfLongLong = sizeof(e);
+	int sizeOfFloat = sizeof(f);
+	int sizeOfDouble = sizeof(g);
 	printf("The result is: \n"
 		"The \'char\' variable is %c, it takes %i %s.\n"
 		"The \'short\' variable is %hi, it takes %i %s.\n"
@@ -38,14 +38,14 @@ int main() {
 		"The \'long\' variable is %li, it takes %i %s.\n"
 		"The \'long long\' variable is %lli, it takes %i %s.\n"
 		"The \'float\' variable is %f, it takes %i %s.\n"
-		"The \'double\' variable is %F, it takes %i %s.\n"
-	, character, sizeOfChar, sizeOfChar > 1 ? PLURAL_FORM : SINGLE_FORM, 
-	shortInteger, sizeOfShort, sizeOfShort > 1 ? PLURAL_FORM : SINGLE_FORM, 
-	integer, sizeOfInt, sizeOfInt > 1 ? PLURAL_FORM : SINGLE_FORM,
-	longInteger, sizeOfLong, sizeOfLong > 1 ? PLURAL_FORM : SINGLE_FORM, 
-	longLongInteger, sizeOfLongLong, sizeOfLongLong > 1 ? PLURAL_FORM : SINGLE_FORM,
-	floatNumber, sizeOfFloat, sizeOfFloat > 1 ? PLURAL_FORM : SINGLE_FORM,
-	doubleNumber, sizeOfDouble, sizeOfDouble > 1 ? PLURAL_FORM : SINGLE_FORM
+		"The \'double\' variable is %F, it takes %i %s.\n", 
+    a, sizeOfChar, sizeOfChar > 1 ? PLURAL_FORM : SINGLE_FORM,
+	b, sizeOfShort, sizeOfShort > 1 ? PLURAL_FORM : SINGLE_FORM, 
+	c, sizeOfInt, sizeOfInt > 1 ? PLURAL_FORM : SINGLE_FORM,
+	d, sizeOfLong, sizeOfLong > 1 ? PLURAL_FORM : SINGLE_FORM, 
+	e, sizeOfLongLong, sizeOfLongLong > 1 ? PLURAL_FORM : SINGLE_FORM,
+	f, sizeOfFloat, sizeOfFloat > 1 ? PLURAL_FORM : SINGLE_FORM,
+	g, sizeOfDouble, sizeOfDouble > 1 ? PLURAL_FORM : SINGLE_FORM
 	);
 	return 0;
 }
