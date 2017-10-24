a2bbf795ea52f30e61c74619f924b30a570838df
FredericDT <frederic.dt.twh@gmail.com>
2017-10-24 16:32:13 +0800

    bug fix

--- C01E03/Problem07.c
+++ C01E03/Problem07.c
@@ -17,7 +17,7 @@ int main() {
     }
     //printf("%s",input);
     int sumOfEngChar = 0, sumOfSpace = 0, sumOfNumber = 0, sumOfOther = 0, index = 0;
-    for (index = 0; index < length; ) {
+    for (index = 0; index < length; index++) {
         if (('a' <= input[index] && input[index] <= 'z') || ('A' <= input[index] && input[index] <= 'Z')) {
             sumOfEngChar++;
         } else if (' ' == input[index]) {
@@ -27,9 +27,8 @@ int main() {
         } else {
             sumOfOther++;
         }
-        index++;
     }
-    printf("The number of English characters, spaces, numbers, other characters are: %d %d %d %d",
+    printf("%d %d %d %d",
            sumOfEngChar, sumOfSpace, sumOfNumber, sumOfOther);
     return 0;
 }
