824236a7ca8461e8f48143ba3daf4c4dca1a1a78
FredericDT <frederic.dt.twh@gmail.com>
2018-06-14 10:40:08 +0800

    Update itos.h

--- TODachr/itos.h
+++ TODachr/itos.h
@@ -1,13 +1,13 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 namespace fdt {
     int intL(unsigned long i) {
         return i >= 10 ? intL(i / 10) + 1 : 1;
     }
-
-    // notify: you need to free the returned pointer to avoid a memory leak
     char * itos(unsigned long i) {
         int l = intL(i);
         char *r = (char *) malloc(sizeof(char) * l + 1);
@@ -18,4 +18,25 @@ namespace fdt {
         }
         return r;
     }
+    char * fdt_strcat(int src_c, char *h, const char *v, ...) {
+        strcat(h, v);
+        va_list valist;
+        va_start(valist, src_c);
+        //char *r;
+        for (int i = 0; i < src_c; i++) {
+            strcat(h, va_arg(valist, const char *));
+        }
+        va_end(valist);
+        return h;
+    }
+}
+
+// test case for fdt_strcat
+int main() {
+    char *a = (char *) malloc(sizeof(char) * 10000);
+
+    char *r = fdt::fdt_strcat(3, a, "first ", "second ", "third ", "end.");
+    printf("%s", a);
+    free(a);
+
 }
