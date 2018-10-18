370a09dd46fdfa7465e7b6502f2baf50a38f2552
FredericDT <frederic.dt.twh@gmail.com>
2018-10-18 13:20:58 +0800

    add shit solution

--- ComputerSystemAssignments/p90/2.68.c
+++ ComputerSystemAssignments/p90/2.68.c
@@ -6,6 +6,16 @@ int lower_one_mask(int n) {
 	return r; 
 }
 
+/*
+int lower_one_mask(int n) {
+	int r = 1;
+	n == 2 && r = 3;
+	n == 3 && r = 7;
+	...
+	return r;
+}
+*/
+
 int main() {
 	printf("%X", lower_one_mask(17));
 	return 0;
