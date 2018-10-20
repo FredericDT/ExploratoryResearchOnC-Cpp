5a140392c763934d1b971bff25bf0a2f8cbe2260
Frederic DT <i@fdt.onl>
2018-10-20 20:36:24 +0800

    Datastruct Assignment 20181020

---
+++ DatastructAssignments/20181020/main.cpp
@@ -0,0 +1,79 @@
+#include <cstdio>
+#include <iostream>
+
+template<typename T>
+struct node {
+    T value;
+    struct node *next;
+};
+
+template<typename T>
+int remove(struct node<T> *n, int k1, int k2) {
+    if (k1 > k2) {
+        return 1;
+    }
+    if (k1 == k2) {
+        return 0;
+    }
+    --k1;
+    int i = 0;
+    while (n->next && i < k1) {
+        n = n->next;
+        ++i;
+    }
+    if (i != k1) {
+        return 2;
+    }
+    while (n->next && i < k2) {
+        n->next = n->next->next;
+        n = n->next;
+        ++i;
+    }
+    if (i != k2) {
+        return 2;
+    }
+    return 0;
+}
+
+template<typename T>
+struct node<T> *create(T e[], int size) {
+    struct node<T> *head = new struct node<T>();
+    struct node<T> *c = head;
+    for (int i = 0; i < size; ++i) {
+        c->next = new struct node<T>();
+        c = c->next;
+        c->value = e[i];
+    }
+    return head;
+}
+
+template<typename T>
+void reverse(T e[], int n) {
+    int i = 0;
+    --n;
+    while (i < n) {
+        T t = e[i];
+        e[i] = e[n];
+        e[n] = t;
+        ++i;
+        --n;
+    }
+}
+
+template<typename T>
+void freeLinkedList(struct node<T> *n) {
+    if (n->next) {
+        freeLinkedList(n->next);
+    }
+    delete n;
+}
+
+int main(int argc, char **argv) {
+    int i[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    struct node<int> *l = create(i, 10);
+    remove(l, 2, 5);
+    reverse(i, 10);
+    freeLinkedList(l);
+    std::cout << "hi";
+    return 0;
+}
