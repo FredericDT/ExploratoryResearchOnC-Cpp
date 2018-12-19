db2ef85432c42ff7ccfcdc3fe6dec5c7b328d2e8
FredericDT <frederic.dt.twh@gmail.com>
2018-12-20 00:21:43 +0800

    Init datas Add init main.cpp Inserted assignments as comments in main.cpp
    I would like to use dijikstra algorightm to compute all distances
    
    	new file:   Edges
    	new file:   Vertexs
    	new file:   main.cpp

---
+++ DatastructAssignments/CampusMapDijikstra/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+/**
+ * @Assignment
+ *
+ * 绘制学校地图；
+ * 将学校的所有建筑物当作节点，两个建筑物间如果直接有路相连则这两个节点间有一条边，路径的长度或者步行所化时间作为边的权值；
+ * 用户输入任意两个建筑物，可以计算出它们之间的最短距离，并给出最短路径
+ * 需要提交设计报告和源代码，要求和《实验一》一样
+ * 补充：给出建筑物内各个位置之间的最短路径；
+ *
+ */
+
+namespace fdt {
+    class Map {
+    public:
+        static class Vertex {
+
+        };
+        static class Edge {
+
+        };
+    };
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
